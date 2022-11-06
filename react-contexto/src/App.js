1c2fd258e4468496014b1a4551fdcee35a009ab2
Ruiz, Francisco Mauricio <maumarruiz@hotmail.com>
2022-11-06 17:44:46 -0300

    Context API

---
+++ react-contexto/src/App.js
@@ -0,0 +1,21 @@
+import MyPage from "./components/MyPage";
+
+function App() {
+  return (
+    <div>
+      <h1>React Context API</h1>
+      <a
+        href="https://es.reactjs.org/docs/context.html"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Documentación
+      </a>
+      <hr />
+      <MyPage />
+      <hr />
+    </div>
+  );
+}
+
+export default App;
---
+++ react-contexto/src/components/MyPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const MyPage = () => {
+  return (
+    <div>
+      <h2>Mi Página</h2>
+    </div>
+  );
+};
+
+export default MyPage;
