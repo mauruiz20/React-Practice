af76b4ec031be90edaa5f1a9f64c1832fffea0c5
Ruiz, Francisco Mauricio <maumarruiz@hotmail.com>
2022-11-06 20:44:13 -0300

    Reducers Hook useReducer

---
+++ react-reducers/src/App.js
@@ -0,0 +1,14 @@
+import Contador from "./components/Contador";
+
+function App() {
+  return (
+    <div>
+      <h1>useReducer</h1>
+      <hr />
+      <Contador />
+      <hr />
+    </div>
+  );
+}
+
+export default App;
