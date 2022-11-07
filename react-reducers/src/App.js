928227f6d0e659047c35b1cca7d87fbaa50f577b
Ruiz, Francisco Mauricio <maumarruiz@hotmail.com>
2022-11-06 21:51:55 -0300

    Shopping Cart Exercise

--- react-reducers/src/App.js
+++ react-reducers/src/App.js
@@ -1,10 +1,13 @@
 import Contador from "./components/Contador";
 import ContadorMejorado from "./components/ContadorMejorado";
+import ShoppingCart from "./components/ShoppingCart";
 
 function App() {
   return (
     <div>
       <h1>useReducer</h1>
+      <hr />
+      <ShoppingCart />
       <hr />
       <ContadorMejorado />
       <hr />
